50dd00951178f219f763364cd7a4feb2df38c196
Neil Woodhouse <clonesupreme@gmail.com>
2021-08-02 15:47:18 +0100

    Added Recipe model

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+class Keyword(models.model):
+    recipe = models.ForeignKey()
+
+class NutritionalInfo(models.model):
+    calories = models.IntegerField()
+
+
 # Create your models here.
+class Recipe(models.model):
+    
+    webAddress = models.URLField()
+    name = models.TextField()
+    description = models.TextField()
+    image = models.models.URLField()
+
+    author = models.TextField()
+    datePublished = models.models.DateField()
+    publisher = models.TextField()
+
+    estimatedCost = models.DecimalField()
+    prepTime = models.DurationField()
+    totalTime = models.DurationField()
+
+    cookTime = models.DurationField()
+    cookingMethod = models.TextField()
+    nutrition = models.ForeignKey(NutritionalInfo)
+    recipeCategory = models.TextField()
+    recipeCuisine = models.TextField()
+    recipeIngredient = models.TextField() # may cause issues later, have to check
+    recipeInstructions = models.TextField()
+    suitableForDiet = models.TextField() # need to process enums for storage
+
+    # keywords = models.ForeignKey() probably not needed
