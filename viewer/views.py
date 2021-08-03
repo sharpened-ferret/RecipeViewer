b59c8613336e36f0bab4b48fc56f8d787d29aa5d
Neil Woodhouse <clonesupreme@gmail.com>
2021-08-03 12:59:08 +0100

    Started work on recipe view
    This view will display registered recipes from the database

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Recipe
+
 def index(request):
     return HttpResponse("Hello world")
+
+def recipe(request):
+    template = loader.get_template('viewer/index.html')
+    context = {}
+    return render(request, 'viewer/index.html', context)
