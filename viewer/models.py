99be805427b03b385d64ce1676a149c633656818
Neil Woodhouse <clonesupreme@gmail.com>
2021-08-05 18:15:29 +0100

    Updated db model to allow for null fields

--- viewer/models.py
+++ viewer/models.py
@@ -21,27 +21,27 @@ class Recipe(models.Model):
     webAddress = models.URLField()
     name = models.TextField()
     description = models.TextField()
-    image = models.URLField()
+    image = models.URLField(blank=True, null=True)
 
      # From Creative Work schema
     author = models.TextField()
     datePublished = models.DateField()
-    publisher = models.TextField()
+    publisher = models.TextField(blank=True, null=True)
 
      # From How To schema
-    estimatedCost = models.DecimalField(max_digits = 5, decimal_places = 2)
-    prepTime = models.DurationField()
-    totalTime = models.DurationField()
+    estimatedCost = models.DecimalField(max_digits = 5, decimal_places = 2, blank=True, null=True)
+    prepTime = models.DurationField(blank=True, null=True)
+    totalTime = models.DurationField(blank=True, null=True)
 
      # From Recipe schema
-    cookTime = models.DurationField()
-    cookingMethod = models.TextField()
-    nutrition = models.ForeignKey(NutritionalInfo, on_delete=models.CASCADE)
-    recipeCategory = models.TextField()
-    recipeCuisine = models.TextField()
+    cookTime = models.DurationField(blank=True, null=True)
+    cookingMethod = models.TextField(blank=True, null=True)
+    nutrition = models.ForeignKey(NutritionalInfo, on_delete=models.CASCADE, blank=True, null=True)
+    recipeCategory = models.TextField(blank=True, null=True)
+    recipeCuisine = models.TextField(blank=True, null=True)
     recipeIngredient = models.TextField() # may cause issues later, have to check
     recipeInstructions = models.TextField()
-    suitableForDiet = models.TextField() # need to process enums for storage
+    suitableForDiet = models.TextField(blank=True, null=True) # need to process enums for storage
 
     dateSaved = models.DateTimeField()
 
