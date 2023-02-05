4799a6dc3716a8a96482188fe140099b1c6ab41d
Neil Woodhouse <clonesupreme@gmail.com>
2023-02-05 14:59:40 +0000

    Changed app url-path to start at root of web address

--- RecipeViewer/urls.py
+++ RecipeViewer/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('viewer/', include('viewer.urls')),
+    path('', include('viewer.urls')),
     path('admin/', admin.site.urls),
 ]
