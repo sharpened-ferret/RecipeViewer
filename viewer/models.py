57709befbf1863953f1b20b0496da48e95a79df0
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-02-08 12:17:45 +0000

    Model doc commenting

--- viewer/models.py
+++ viewer/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from django.forms import ModelForm
 
-# Create your models here.
+# Defines a recipe data model
+# based on https://schema.org/Recipe
 class Recipe(models.Model):
     def __str__(self):
         return self.name
     
      # From Thing schema
+     # https://schema.org/Thing
     webAddress = models.URLField()
     name = models.TextField()
     description = models.TextField(blank=True, null=True)
     image = models.TextField(blank=True, null=True)
 
      # From Creative Work schema
+     # https://schema.org/CreativeWork
     author = models.TextField()
     datePublished = models.DateField(blank=True, null=True)
     publisher = models.TextField(blank=True, null=True)
 
      # From How To schema
+     # https://schema.org/HowTo
     estimatedCost = models.DecimalField(max_digits = 5, decimal_places = 2, blank=True, null=True)
     prepTime = models.DurationField(blank=True, null=True)
     totalTime = models.DurationField(blank=True, null=True)
@@ -31,14 +35,18 @@ class Recipe(models.Model):
     recipeInstructions = models.TextField()
     suitableForDiet = models.TextField(blank=True, null=True) # need to process enums for storage
 
+    # Non-schema - added for app functionality
     dateSaved = models.DateTimeField()
 
+# Form for manual entry of Recipe data
 class AddRecipeManual(ModelForm):
     class Meta:
         model = Recipe
         fields = '__all__'
         
 
+# Describes a recipe's nutritional information
+# based on https://schema.org/NutritionInformation
 class NutritionalInfo(models.Model):
     def __str__(self):
         return self.recipe.name + " nutritional info"
@@ -57,7 +65,8 @@ class NutritionalInfo(models.Model):
     transFatContent = models.TextField(blank=True, null=True)
     unsaturatedFatContent = models.TextField(blank=True, null=True)
 
-
+# Keywords map keyword -> Recipe
+# Used to improve recipe searching
 class Keyword(models.Model):
     def __str__(self):
         return self.keyword + " | " + self.recipe.name
